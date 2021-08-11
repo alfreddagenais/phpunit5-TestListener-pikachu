a8fa7e0d13e4ba4126b3c4886a2e95ad3f557438
Alfred Dagenais <jesuis@alfreddagenais.com>
2021-08-11 19:33:46 -0400

    🚧  Adding phpcs Standard Coding

--- tests/php/HelperAverageTest.php
+++ tests/php/HelperAverageTest.php
@@ -5,20 +5,47 @@ use Framework\Core\HelperAverage;
 class HelperAverageTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * Set UpTest
+     *
+     * @return void
+     */
     public function setUp()
     {
-        // nothing to Setup
+        // Nothing to Setup
     }
 
+    /**
+     * Test CalculationOfMean
+     *
+     * @return void
+     */
     public function testCalculationOfMean()
     {
-        $numbers = [3, 7, 6, 1, 5];
+        $numbers = [
+            3,
+            7,
+            6,
+            1,
+            5,
+        ];
         $this->assertEquals(4.4, HelperAverage::mean($numbers));
     }
 
+    /**
+     * Test Calculation Of Median
+     *
+     * @return void
+     */
     public function testCalculationOfMedian()
     {
-        $numbers = [3, 7, 6, 1, 5];
+        $numbers = [
+            3,
+            7,
+            6,
+            1,
+            5,
+        ];
         $this->assertEquals(5, HelperAverage::median($numbers));
     }
 }
--- tests/php/TestListener.php
+++ tests/php/TestListener.php
@@ -3,66 +3,157 @@
 class TestListener extends PHPUnit_Framework_BaseTestListener
 {
 
-    private $_nTestStartedTotal = 0;
-    private $_nTestEndedTotal = 0;
-    private $_nTestErrorTotal = 0;
+    /** @var integer */
+    private $testStartedTotal = 0;
 
+    /** @var integer */
+    private $testEndedTotal = 0;
+
+    /** @var integer */
+    private $testErrorTotal = 0;
+
+    /**
+     * Class Constructor
+     */
     public function __construct()
     {
     } // does nothing but throws an error if not here
 
+    /**
+     * Add Error
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     * @param Exception              $e    Exception
+     * @param float                  $time time
+     *
+     * @return void
+     */
     public function addError(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
         echo "\n";
         printf("Error while running test '%s'.\n", $test->getName());
-        $this->_nTestErrorTotal++;
+        $this->testErrorTotal++;
     }
 
+    /**
+     * Add Warning
+     *
+     * @param PHPUnit_Framework_Test    $test Test
+     * @param PHPUnit_Framework_Warning $e    Warning
+     * @param float                     $time time
+     *
+     * @return void
+     */
     public function addWarning(PHPUnit_Framework_Test $test, PHPUnit_Framework_Warning $e, $time)
     {
         echo "\n";
         printf("Warning while running test '%s'.\n", $test->getName());
     }
 
+    /**
+     * Add Failure
+     *
+     * @param PHPUnit_Framework_Test                 $test Test
+     * @param PHPUnit_Framework_AssertionFailedError $e    AssertionFailedError
+     * @param float                                  $time time
+     *
+     * @return void
+     */
     public function addFailure(PHPUnit_Framework_Test $test, PHPUnit_Framework_AssertionFailedError $e, $time)
     {
         echo "\n";
         printf("Test '%s' failed.\n", $test->getName());
-        $this->_nTestErrorTotal++;
+        $this->testErrorTotal++;
     }
 
+    /**
+     * Add Incomplete Test
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     * @param Exception              $e    Exception
+     * @param float                  $time time
+     *
+     * @return void
+     */
     public function addIncompleteTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
         printf("Test '%s' is incomplete.\n", $test->getName());
     }
 
+    /**
+     * Add Risky Test
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     * @param Exception              $e    Exception
+     * @param float                  $time time
+     *
+     * @return void
+     */
     public function addRiskyTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
         printf("Test '%s' is deemed risky.\n", $test->getName());
     }
 
+    /**
+     * Add Skipped Test
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     * @param Exception              $e    Exception
+     * @param float                  $time time
+     *
+     * @return void
+     */
     public function addSkippedTest(PHPUnit_Framework_Test $test, Exception $e, $time)
     {
-        //printf("Test '%s' has been skipped.\n", $test->getName());
+        // Ex.: // printf("Test '%s' has been skipped.\n", $test->getName());
     }
 
+    /**
+     * Start Test
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     *
+     * @return void
+     */
     public function startTest(PHPUnit_Framework_Test $test)
     {
-        //printf("Test '%s' started.\n", $test->getName());
-        $this->_nTestStartedTotal++;
+        // Ex.: // printf("Test '%s' started.\n", $test->getName());
+        $this->testStartedTotal++;
     }
 
+    /**
+     * End Test
+     *
+     * @param PHPUnit_Framework_Test $test Test
+     * @param float                  $time time
+     *
+     * @return void
+     */
     public function endTest(PHPUnit_Framework_Test $test, $time)
     {
-        // printf("Test '%s' ended.\n", $test->getName());
-        $this->_nTestEndedTotal++;
+        // Ex.: // printf("Test '%s' ended.\n", $test->getName());
+        $this->testEndedTotal++;
     }
 
+    /**
+     * Start TestSuite
+     *
+     * @param PHPUnit_Framework_TestSuite $suite Suite
+     *
+     * @return void
+     */
     public function startTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
-        // printf("TestSuite '%s' started.\n", $suite->getName());
+        // Ex.: // printf("TestSuite '%s' started.\n", $suite->getName());
     }
 
+    /**
+     * EndTest Suite
+     *
+     * @param PHPUnit_Framework_TestSuite $suite Suite
+     *
+     * @return void
+     */
     public function endTestSuite(PHPUnit_Framework_TestSuite $suite)
     {
         $sSuiteName = trim($suite->getName());
@@ -70,11 +161,11 @@ class TestListener extends PHPUnit_Framework_BaseTestListener
             return;
         }
 
-        if ($suite->count() != $this->_nTestStartedTotal) {
-          return;
+        if ($suite->count() != $this->testStartedTotal) {
+            return;
         }
 
-        if ($this->_nTestErrorTotal > 0) {
+        if ($this->testErrorTotal > 0) {
             return;
         }
 
